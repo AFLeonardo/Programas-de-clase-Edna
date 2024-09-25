255cd8312a18f69704028d0f208d121be1f31722
Leonardo <alfonsorruiz012@gmail.com>
2024-09-25 10:55:06 -0600

    Ejercicio 6 funcionando correctamente.

--- Etapa 2/Ejercicio 6.c
+++ Etapa 2/Ejercicio 6.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-// Utilizar codigo ASCII
-bool ValidarLetraEnString(char *, char *);
+
+// PROTOTIPO de funciones
+bool ValidarLetraEnString(char *string, char *letra);
 bool ValidarCadena(char *cadena);
 
 int main()
 {
-    char cadena[100],
-         letra;
+    char cadena[100], letra;
     bool encontrada;
 
     do
     {
         printf("Ingrese una cadena de caracteres: ");
-        gets(cadena);
-        // Validar con codigo ASCII
-        if(!ValidarCadena(cadena))
-            printf("ERROR. \nCadena no valida.\n")
+        gets(cadena); 
+        // Validar la cadena con el código ASCII
+        if (!ValidarCadena(cadena))
+            printf("ERROR. \nCadena no valida.\n");
 
     } while (!ValidarCadena(cadena));
 
@@ -25,36 +25,37 @@ int main()
     fflush(stdin);
     scanf(" %c", &letra);
 
-    if (islower(letra))
-        encontrada = ValidarLetraEnString(&cadena, &letra);
-    else
-        encontrada = ValidarLetraEnString(&cadena, toupper(&letra));
-
+    encontrada = ValidarLetraEnString(cadena, &letra);
 
     printf("La letra \"%c\" %s en la cadena \n\"%s\"\n", letra, encontrada ? "esta" : "no esta", cadena);
     system("pause");
     return 0;
 }
 
-bool ValidarLetraEnString(char *string, char letra)
+
+bool ValidarLetraEnString(char *string, char *letra)
 {
     int i = 0;
     while (string[i] != '\0')
     {
-        if (string[i] == letra)
+        if (string[i] == *letra)
             return true;
         i++;
     }
     return false;
 }
 
+
 bool ValidarCadena(char *cadena) 
 {
-    bool valido = false;
-    while (*cadena != "\0" || (*cadena > 65 || *cadena <= 90)) 
+    while (*cadena != '\0') 
     {
-        if (!isalpha(*cadena) && !isspace(*cadena)) {
-            return false; 
+        
+        if (!((*cadena >= 'A' && *cadena <= 'Z') || 
+              (*cadena >= 'a' && *cadena <= 'z') || 
+              *cadena == ' '))
+        {
+            return false;
         }
         cadena++;
     }
